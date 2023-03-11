5fe1814124c9e5e3ed8617414bf1f86e5db261bb
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-03-11 18:28:52 -0300

    Ajuste

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -3,4 +3,4 @@ SELECT	MIN(SpotifyClone.plano.plano_valor) AS 'faturamento_minimo',
 		ROUND(AVG(SpotifyClone.plano.plano_valor), 2) AS 'faturamento_medio',
 		SUM(SpotifyClone.plano.plano_valor) AS 'faturamento_total'
 FROM SpotifyClone.plano
-INNER JOIN SpotifyClone.usuarios ON SpotifyClone.plano.plano_id = SpotifyClone.usuarios.plano_id;
+JOIN SpotifyClone.usuarios ON SpotifyClone.plano.plano_id = SpotifyClone.usuarios.plano_id;
